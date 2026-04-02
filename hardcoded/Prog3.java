286d72e47cd1954818d801e9f334561161cff522
Gabe <venegasg1@arizona.edu>
2026-04-02 00:38:45 -0700

    final submission

---
+++ hardcoded/Prog3.java
@@ -0,0 +1,308 @@
+
+import java.io.*;
+import java.sql.*;
+
+/**
+ * File: Prog3.java
+ * Author: Gabe Venegas
+ * Course: CSC 460
+ * Desc: Command line interface for SQL database using JDBC.
+ * Containing specific commands for views on highwayrail data.
+ * Each tuple in highwayrail tables are arranged as follows:
+ * 
+ * railroad_code                VARCHAR2(30)
+ * incident_number              VARCHAR2(30)
+ * grade_crossing_id            VARCHAR2(30)
+ * date_time                    TIMESTAMP
+ * state_name                   VARCHAR2(30)
+ * highway_user                 VARCHAR2(30)
+ * temperature                  INT
+ * visibility                   VARCHAR2(30)
+ * weather_condition            VARCHAR2(30)
+ * number_of_locomotive_units   INT
+ * number_of_cars               INT
+ * 
+ * NOTE: Each highwayrail table is named after the year of 
+ * its entries, and collected by db user (e.g. STUDENT_NAME.HWY_YEAR)
+ */
+public class Prog3 {
+
+    // Get number of reports (row count) per year (table)
+    public static void executeA(Connection dbconn) throws SQLException {
+
+        String sql = "SELECT '1980' AS table_year, COUNT(*) AS row_count FROM venegasg1.\"1980\" "
+                + "UNION "
+                + "SELECT '1995' AS table_year, COUNT(*) AS row_count FROM venegasg1.\"1995\" "
+                + "UNION "
+                + "SELECT '2010' AS table_year, COUNT(*) AS row_count FROM venegasg1.\"2010\" "
+                + "UNION "
+                + "SELECT '2025' AS table_year, COUNT(*) AS row_count FROM venegasg1.\"2025\" ";
+
+        Prog3.executeQuery(sql, dbconn);
+    }
+
+    // Display incident count by state (top 10, descending) for given year
+    public static void executeB(Connection dbconn, String year) throws SQLException {
+
+        String sql = "SELECT * FROM ("
+                + "SELECT state_name, COUNT(*) AS incident_count FROM venegasg1.\"" + year + "\" "
+                + "GROUP BY state_name "
+                + "ORDER BY incident_count DESC "
+                + ") WHERE ROWNUM <= 10";
+
+        Prog3.executeQuery(sql, dbconn);
+    }
+
+    // Display largest % drop in incident count by state (top 5, descending) between given years
+    public static void executeC(Connection dbconn, String year1, String year2) throws SQLException {
+
+        String sql = "SELECT * FROM ("
+                + "SELECT y1.state_name, "
+                + "     y1.incident_count AS incidents_" + year1 + ", "
+                + "     y2.incident_count AS incidents_" + year2 + ", "
+                + "     ROUND(100.0 * ((y1.incident_count - y2.incident_count) / (1.0 * y1.incident_count)), 2) AS percent_drop "
+                + "FROM (SELECT state_name, COUNT(*) AS incident_count FROM venegasg1.\"" + year1
+                + "\" GROUP BY state_name) y1 "
+                + "JOIN (SELECT state_name, COUNT(*) AS incident_count FROM venegasg1.\"" + year2
+                + "\" GROUP BY state_name) y2 "
+                + "ON y1.state_name = y2.state_name "
+                + "ORDER BY percent_drop DESC "
+                + ") WHERE ROWNUM <= 5";
+
+        Prog3.executeQuery(sql, dbconn);
+    }
+
+    // Display largest % drop in incident count by field (top 10, descending) between given years
+    public static void executeD(Connection dbconn, String year1, String year2, String field) throws SQLException {
+        
+        String sql = "SELECT * FROM ("
+                + "SELECT y1." + field + ", "
+                + "     y1.incident_count AS incidents_" + year1 + ", "
+                + "     y2.incident_count AS incidents_" + year2 + ", "
+                + "     ROUND(100.0 * ((y1.incident_count - y2.incident_count) / (1.0 * y1.incident_count)), 2) AS percent_drop "
+                + "FROM (SELECT " + field + ", COUNT(*) AS incident_count FROM venegasg1.\"" + year1
+                + "\" GROUP BY " + field + ") y1 "
+                + "JOIN (SELECT " + field + ", COUNT(*) AS incident_count FROM venegasg1.\"" + year2
+                + "\" GROUP BY " + field + ") y2 "
+                + "ON y1." + field + " = y2." + field + " "
+                + "ORDER BY percent_drop DESC "
+                + ") WHERE ROWNUM <= 10";
+
+        Prog3.executeQuery(sql, dbconn);
+    }
+
+    // Gets nice String from ResultSet for printing 
+    public static String resultSetToString(ResultSet rs) throws SQLException {
+        ResultSetMetaData meta = rs.getMetaData();
+        int columnCount = meta.getColumnCount();
+        StringBuilder sb = new StringBuilder();
+
+        // Print column headers
+        for (int i = 1; i <= columnCount; i++) {
+            sb.append(meta.getColumnLabel(i));
+            if (i < columnCount)
+                sb.append("\t");
+        }
+        sb.append("\n\n");
+
+        // Iterate through rows
+        while (rs.next()) {
+            for (int i = 1; i <= columnCount; i++) {
+                int type = meta.getColumnType(i);
+
+                // Let JDBC figure out type mapping
+                Object value = rs.getObject(i);
+
+                // Handle SQL NULL
+                if (rs.wasNull()) {
+                    sb.append("NULL");
+                } else {
+                    sb.append(value);
+                }
+
+                // Delimit cols
+                if (i < columnCount)
+                    sb.append("\t");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    // Wrapper of Statement.executeQuery for printing & autoclose
+    public static void executeQuery(String query, Connection dbconn) throws SQLException {
+
+        try (Statement stmt = dbconn.createStatement();
+                ResultSet answer = stmt.executeQuery(query)) {
+
+            if (answer != null) {
+                System.out.println(Prog3.resultSetToString(answer));
+            }
+
+        }
+    }
+
+    // Wrapper of Statement.executeUpdate for printing & autoclose
+    public static void executeUpdate(String sql, Connection dbconn) throws SQLException {
+        try (Statement stmt = dbconn.createStatement()) {
+
+            int rowsAffected = stmt.executeUpdate(sql);
+            System.out.println("Rows updated: " + rowsAffected);
+
+        }
+    }
+
+    // prints helpful CLI commands to user
+    public static void printHelp() {
+        System.out.println(
+            "\n\t--------------------------------------------------------------\n\n"
+            + "\t(All other input will be ran as SQL statements w/ output)\n\n"
+            + "\t?tuple                     - Display highwayrail tuple fields & types\n\n"
+            + "\t?a                         - Display incident count by year\n\n"
+            + "\t?b <year>                  - Display incident count by state \n"
+            + "\t                             (top 10, descend) for given year\n\n"
+            + "\t?c <year1> <year2>         - Display largest % drop in \n"
+            + "\t                             incident count by state (top 5, \n"
+            + "\t                             descend) between given years\n\n"
+            + "\t?d <year1> <year2> <field> - Display largest % drop in \n"
+            + "\t                             incident count by field (top 10, \n"
+            + "\t                             descend) between given years\n\n"
+            + "\t--------------------------------------------------------------\n\n"
+            + "\tNOTE:\n\n"
+            + "\tCourse TAs can find my tables explicitly under \"venegasg1\":\n\n"
+            + "\t\tvenegasg1.\"1980\"\n"
+            + "\t\tvenegasg1.\"1995\"\n"
+            + "\t\tvenegasg1.\"2010\"\n"
+            + "\t\tvenegasg1.\"2025\"\n\n"
+            + "\t--------------------------------------------------------------\n");
+                
+    }
+
+    public static void main(String[] args) {
+
+        String username = null, password = null;
+        Connection dbconn = null;
+
+        // Check args
+
+        if (args.length == 2) { // get credentials from user args
+            username = args[0];
+            password = args[1];
+        } else {
+            System.out.println("\nUsage: java Prog3 <username> <password>\n\n"
+                    + "\t<username> : Database username\n\n"
+                    + "\t<password> : Database password\n");
+            System.exit(-1);
+        }
+
+        // Check valid drivers
+
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+
+            String dbURL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+
+            dbconn = DriverManager.getConnection(dbURL, username, password);
+
+        } catch (ClassNotFoundException e) {
+            System.err.println("*** ClassNotFoundException:  "
+                    + "Error loading JDBC driver \"oracle.jdbc.OracleDriver\"\n"
+                    + "\tPerhaps the driver is not on the Classpath?\n\n"
+                    + "For CSC 460 on Lectura, run:\n"
+                    + "\texport CLASSPATH=/usr/lib/oracle/19.8/client64/lib/ojdbc8.jar:${CLASSPATH}");
+            System.exit(-1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("Unable to get connection to DB");
+        }
+
+        // Begin CLI loop
+
+        String query;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Prog3.printHelp();
+
+        while (true) {
+            System.out.print("Enter a command (or 'help', 'exit'): ");
+
+            try {
+
+                query = br.readLine();
+
+                if (query.equalsIgnoreCase("exit"))
+                    break;
+
+
+                else if (query.equalsIgnoreCase("help"))
+                    Prog3.printHelp();
+
+                else if (query.toLowerCase().startsWith("?tuple"))
+                    System.out.println(
+                        "\n\trailroad_code                VARCHAR2(30)\n"
+                        + "\tincident_number              VARCHAR2(30)\n"
+                        + "\tgrade_crossing_id            VARCHAR2(30)\n"
+                        + "\tdate_time                    TIMESTAMP\n"
+                        + "\tstate_name                   VARCHAR2(30)\n"
+                        + "\thighway_user                 VARCHAR2(30)\n"
+                        + "\ttemperature                  INT\n"
+                        + "\tvisibility                   VARCHAR2(30)\n"
+                        + "\tweather_condition            VARCHAR2(30)\n"
+                        + "\tnumber_of_locomotive_units   INT\n"
+                        + "\tnumber_of_cars               INT\n\n"
+                    );
+
+                else if (query.toLowerCase().startsWith("?a")) {
+                    Prog3.executeA(dbconn);
+
+
+                } else if (query.toLowerCase().startsWith("?b")) {
+                    String year = query.split("\\s+")[1];
+                    Prog3.executeB(dbconn, year);
+
+
+                } else if (query.toLowerCase().startsWith("?c")) {
+                    String year1 = query.split("\\s+")[1];
+                    String year2 = query.split("\\s+")[2];
+                    Prog3.executeC(dbconn, year1, year2);
+
+
+                } else if (query.toLowerCase().startsWith("?d")) {
+                    String year1 = query.split("\\s+")[1];
+                    String year2 = query.split("\\s+")[2];
+                    String field = query.split("\\s+")[3];
+                    Prog3.executeD(dbconn, year1, year2, field);
+
+
+                } else if (query.toLowerCase().startsWith("select"))
+                    Prog3.executeQuery(query, dbconn);
+
+                else
+                    Prog3.executeUpdate(query, dbconn);
+
+
+            } catch (IOException e) {
+                System.err.println("Error reading input: " + e.getMessage());
+
+
+            } catch (SQLException e) {
+                System.err.println("\n*** SQLException:");
+                System.err.println("    Message:   " + e.getMessage());
+                System.err.println("    SQLState:  " + e.getSQLState());
+                System.err.println("    ErrorCode: " + e.getErrorCode() + "\n");
+
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.err.println("Array out of bounds (..possibly missing a command arg?)");
+            }
+        }
+
+        try {
+            br.close();
+            dbconn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
